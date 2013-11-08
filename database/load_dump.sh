7b8ce4cbdbce02eae392e79a8be4d59c614dfaa9
ggoral <goral.gonzalo@gmail.com>
2013-11-07 22:42:30 -0300

    add feature load_dump.sh

---
+++ database/load_dump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MYSQL_HOSTNAME="172.16.207.133"
+MYSQL_USERNAME="ggoral" 
+MYSQL_DATABASE="fba_db"
+
+#uncomment next line
+#mysql -h $MYSQL_HOSTNAME -u $MYSQL_USERNAME -p $MYSQL_DATABASE < $1
+
+#Add privileges to user mysql
+#
+#
