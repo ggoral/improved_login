28e7d502dacd7ab485abb8184e2b0458e126c366
jsivori <jonatansivori@gmail.com>
2013-11-08 22:41:25 -0300

    Subo el modelo decision con los cambios propuestos para la insercion teniendo en cuenta la relacion con el analito

--- model/decision_interface_test.php
+++ model/decision_interface_test.php
@@ -2,16 +2,18 @@
 
 require_once "decision_interface.php";
 
-$decision = ORM_decision::buscar_decision(13);
+//$decision = ORM_decision::buscar_decision(13);
 
-//$affected_row = ORM_decision::agregar_decision("Gonzalo");
+$affected_row = ORM_decision::agregar_decision("Gonzalo");
+
+$affected_row = ORM_decision::combinar_decision_analito("Gonzalo",2);
 //$affected_row = ORM_decision::eliminar_decision(5);
-//echo "Cantidad Afectada:",$affected_row,"\n";
-
-
-$decision->setDescripcion("UN decision MAS 234342345");
-$affected_row = ORM_decision::actualizar_decision($decision);
 echo "Cantidad Afectada:",$affected_row,"\n";
+
+
+//$decision->setDescripcion("UN decision MAS 234342345");
+//$affected_row = ORM_decision::actualizar_decision($decision);
+//echo "Cantidad Afectada:",$affected_row,"\n";
 
 //imprimir todos los decisions
 //$decisions = ORM_decision::obtener_todos_decision();
