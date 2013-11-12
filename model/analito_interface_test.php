1656b3c5cb67773e420c8360680c13c35c568f8b
ggoral <goral.gonzalo@gmail.com>
2013-11-12 19:06:34 -0300

    model/analito_interface_test.php

--- model/analito_interface_test.php
+++ model/analito_interface_test.php
@@ -2,19 +2,19 @@
 
 require_once "analito_interface.php";
 
-$analito = ORM_analito::buscar_analito(6);
-
+$analito = ORM_analito::buscar_analito(1);
+var_dump($analito);
 //$affected_row = ORM_analito::agregar_analito("Gonzalo");
 //$affected_row = ORM_analito::eliminar_analito(5);
 //echo "Cantidad Afectada:",$affected_row,"\n";
 
 
-$analito->setDescripcion("UN ANALITO MAS 234342345");
-$affected_row = ORM_analito::actualizar_analito($analito);
-echo "Cantidad Afectada:",$affected_row,"\n";
+//$analito->setDescripcion("UN ANALITO MAS 234342345");
+//$affected_row = ORM_analito::actualizar_analito($analito);
+//echo "Cantidad Afectada:",$affected_row,"\n";
 
 //imprimir todos los analitos
-$analitos = ORM_analito::obtener_todos_analito();
-print_r($analitos);
+//$analitos = ORM_analito::obtener_todos_analito();
+//print_r($analitos);
 
 ?>
