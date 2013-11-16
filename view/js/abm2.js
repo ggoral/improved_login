650f1ad275d4bac6bb7745be46c200f7b85284bc
jsivori <jonatansivori@gmail.com>
2013-11-16 11:54:53 -0300

    fix

--- view/js/abm2.js
+++ view/js/abm2.js
@@ -29,7 +29,7 @@ function submitearForm(action, tabla){
 				break;
 			case '2':
 				error=true;
-				mensaje='Operacion Fallida - Error al insertar elemento';	
+				mensaje='Operacion Fallida - Error al insertar Analito/s';	
 				break;
 			case '3':
 				error=true;
