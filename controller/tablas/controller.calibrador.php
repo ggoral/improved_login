229dc92b8e100fb1ced60a2e2118641fa1d2ec9a
jsivori <jonatansivori@gmail.com>
2013-11-15 21:11:58 -0300

    tabla

--- controller/tablas/controller.calibrador.php
+++ controller/tablas/controller.calibrador.php
@@ -7,29 +7,7 @@ $parametro_template = 'abm/calibrador.html';
 $parametro_columnas = Array('ID_CALIBRADOR','DESCRIPCION');
 $parametro_datos = ORM_calibrador::obtener_todos_calibrador();
 
-if (isset($_POST['error'])){
-  $error = $_POST['error'];
-  $display = 'block';
-  $color = "red";
-}
-else{
-  $error = '';
-  $display = 'none';
-  $color = '';
-}
-
-if($error == "1"){
-  $color = "green";
-  $error = "Operacion Exitosa!";
-}
-$parametro_display = array(
-    'cabecera' => $parametro_columnas,
-    'filas' => $parametro_datos,
-    'error' => $error,
-    'displayerror' => $display,
-	'messagecolor' => $color
-  );
-
+require 'controller.checkerror.php';
 require '../controller.generico.php';
 
 ?>
