3b1461ff03b71684a73bf1bd4393c0e400e2a28d
Agustin Parmisano <agustinparmisano@gmail.com>
2013-11-15 23:50:11 -0300

    anda perfecto analito y rol, falta arreglar tipo lab y pais

--- controller/consultas/consultas_pais.php
+++ controller/consultas/consultas_pais.php
@@ -6,22 +6,25 @@ require_once '../../model/test_input.php';
 
 if (isset($_POST['action'])){
 	//RECOBRAR LOS DATOS POR EL POST
-	if ((!isset($_POST['descripcion']))or(!test_input($_POST['descripcion']))
-		or (!isset($_POST['id_pais']))or(!test_input($_POST['id_pais'])){
-			die ('5'); //NO PASA VALIDACION DEL LADO DEL SERVIDOR
+	if ((!isset($_POST['descripcion']))or(!test_input($_POST['descripcion']))){
+		die ('5'); //NO PASA VALIDACION DEL LADO DEL SERVIDOR
 	}	
 
 	$descripcion = $_POST['descripcion'];
-	$id_pais = $_POST['id_pais'];
 	
 	if ($_POST['action'] == 'editar'){
-		//HACE EL UPDATE
-		$pais = new pais ();
-		$pais->setId_pais($id_pais);
-		$pais->setDescripcion($descripcion);
-		ORM_pais::actualizar_pais($pais);
-		echo "1";
-		return;
+		$id_pais = $_POST['id_pais'];
+		if((!isset($_POST['id_pais']))or(!test_input($_POST['id_pais']))){
+			die ('5'); //NO PASA VALIDACION DEL LADO DEL SERVIDOR
+		}else{
+			//HACE EL UPDATE
+			$pais = new pais ();
+			$pais->setId_pais($id_pais);
+			$pais->setDescripcion($descripcion);
+			ORM_pais::actualizar_pais($pais);
+			echo "1";
+			return;
+		}
 	}
 	elseif ($_POST['action'] == 'alta'){
 		//HACE EL INSERT
