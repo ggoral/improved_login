90289ce42359e18e67b46f25ec1c25d49b74737d
ggoral <goral.gonzalo@gmail.com>
2013-11-16 21:15:43 -0300

    Inicio de Validar Session

--- controller/validarSesion.php
+++ controller/validarSesion.php
@@ -1,16 +1,22 @@
 <?php
 session_start();
-
-
-if((!isset($_SESSION['usuarioLogeado']['username']) or ($_SESSION['usuarioLogeado']['username'] == '')) or
-   (!isset($_SESSION['usuarioLogeado']['rol']) or ($_SESSION['usuarioLogeado']['rol'] == '')))
-   {
-        header('Location: index.php?error=ERROR DE LOGIN O SESION EXPIRADA');
-   }	
-else{
-	$idRolSesion= $_SESSION['usuarioLogeado']['rol'];
-	$idUsuarioSesion= $_SESSION['usuarioLogeado']['username'];  
-}	
-
+//tiene que estar seteada la variable $rol_session
+ 
+if(isset($_SESSION['usuarioLogeado']['rol']))
+  {
+  $validarRol = substr($_SESSION['usuarioLogeado']['rol'],0,3);
+  if (array_key_exists($validarRol,$tipo_usuario)) 
+    {
+    $idRolSesion= $_SESSION['usuarioLogeado']['rol'];
+    $idUsuarioSesion= $_SESSION['usuarioLogeado']['username'];
+    }
+    else
+    {
+    header('Location: index.php?error=Permisos insuficientes');
+    }
+  }	
+else
+  {
+	header('Location: index.php?error=Inicie Session');
+  }
 ?>
-
