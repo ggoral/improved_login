a84c0e1b3b45aedeae9a3da9fe0cd99db4ca5863
ggoral <goral.gonzalo@gmail.com>
2013-11-17 23:30:39 -0300

    add title

--- controller/tablas/controller.maps.php
+++ controller/tablas/controller.maps.php
@@ -1,8 +1,8 @@
 <?php
 //DRY don't repeat yourself
-//$tipo_usuario = array('Adm','FBA');
+$tipo_usuario = array('Adm','FBA');
 
-//require_once '../validarSesion.php';
+require_once '../validarSesion.php';
 require_once '../../model/laboratorio_interface.php';
 $parametro_template = 'maps.html';
 $parametro_datos = ORM_laboratorio::mostrar_coordenadas();
