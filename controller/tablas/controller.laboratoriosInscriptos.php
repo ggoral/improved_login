ede0aa1903acad3c112d6534768a6717cc83868e
AgustinParmisano <agustinparmisano@gmail.com>
2013-12-09 19:39:02 -0300

    laboratorios inscriptos terminado hasta la vista, revisar la tabla y los campos

---
+++ controller/tablas/controller.laboratoriosInscriptos.php
@@ -0,0 +1,47 @@
+<?php
+//DRY don't repeat yourself
+$tipo_usuario = array('Adm','FBA');
+
+require_once '../validarSesion.php';
+require_once '../../model/laboratorio_interface.php';
+
+$parametro_template = 'abm/laboratorio.html';
+$parametro_columnas = Array('CODIGO','FECHA INGRESO','FECHA BAJA','ANALITO','ENCUESTA','TIPO',);
+$parametro_datos = ORM_laboratorio::mostrar_laboratorios_inscriptos();
+
+
+$perfil = $_SESSION['usuarioLogeado']['rol'];
+//if ($perfil != 'Administrador')
+	//$idLab = str_replace('Laboratorio_','',$perfil);
+
+	
+
+if (isset($_POST['error'])){
+  $error = $_POST['error'];
+  $display = 'block';
+  $color = "red";
+}
+else{
+  $error = '';
+  $color = "yellow";
+  $display = 'none';
+}
+
+if($error == "1"){
+  $color = "green";
+  $error = "Operacion Exitosa!";
+}
+
+$parametro_display = array(
+    'cabecera' => $parametro_columnas,
+    'filas' => $parametro_datos,
+	'perfil' => $perfil,
+    'error' => $error,
+    'displayerror' => $display,
+    'messagecolor' => $color
+  );
+  
+  
+require '../controller.generico.php';
+
+?>
