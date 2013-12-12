fab3754bc87a83874948a40ba26767e12935e038
AgustinParmisano <agustinparmisano@gmail.com>
2013-12-12 18:10:13 -0300

    arreglado analito, arreglado lo del script y anda bien

--- controller/tablas/controller.estadisticasAnalito.php
+++ controller/tablas/controller.estadisticasAnalito.php
@@ -6,7 +6,7 @@ require_once '../validarSesion.php';
 require_once '../../model/analito_interface.php';
 
 $parametro_template = 'abm/analitoEstadistica.html';
-$parametro_columnas = Array('DESCRIPCION','LABORATORIO','CANT ANALITOS');
+$parametro_columnas = Array('LABORATORIO','CANT ANALITOS');
 $parametro_datos = ORM_analito::obtener_analito_laboratorio();
 
 $parametro_display = array(
