23ccca4b0851fe6f1dfa000f99b6c54bb2fb54e1
AgustinParmisano <agustinparmisano@gmail.com>
2013-12-13 17:10:19 -0300

    Agregadas logicas de participantes y comparaciones

---
+++ controller/abm/vista_mostrar_participantes.php
@@ -0,0 +1,17 @@
+<?php
+//DRY don't repeat yourself
+$tipo_usuario = array('Adm','FBA','Lab');
+
+require_once '../validarSesion.php';
+require_once '../../model/encuesta_interface.php';
+
+$parametro_template = 'abm/vista_mostrar_participantes.html';
+$parametro_datos = ORM_encuesta::participante_encuestas($_POST['id_encuesta']);
+
+$parametro_display = array(
+    'encuesta' => $parametro_datos
+  );
+
+require '../controller.generico.php';
+
+?>
