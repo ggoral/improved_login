0999b831463c3c6c3904d0104077a97f7985a8f6
ggoral <goral.gonzalo@gmail.com>
2013-12-13 17:02:58 -0300

    add controller stats

---
+++ controller/tablas/controller.stats.php
@@ -0,0 +1,15 @@
+<?php
+//DRY don't repeat yourself
+$tipo_usuario = array('Adm','FBA');
+
+require_once '../validarSesion.php';
+require_once '../../model/laboratorio_interface.php';
+$parametro_template = 'stats.html';
+$parametro_datos = ORM_laboratorio::mostrar_coordenadas();
+$parametro_columnas = Array();
+$parametro_display = array(
+    'laboratorios' => $parametro_datos,
+  );
+
+require '../controller.generico.php';
+?>
