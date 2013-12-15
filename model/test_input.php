127bd3546a0885ea55260f8825cb11911241d0bc
ggoral <goral.gonzalo@gmail.com>
2013-12-15 15:20:38 -0300

    remove mysql_real_escape_string(): The mysql extension

--- model/test_input.php
+++ model/test_input.php
@@ -4,7 +4,6 @@ function test_input($data)
   {
   $data = trim($data);
   $data = stripslashes($data);
-  $data = mysql_real_escape_string($data);
   $data = htmlspecialchars($data);
   return $data;
   }
