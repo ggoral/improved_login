2815fc11464b652a46ec569dd26eab9b65cced07
ggoral <goral.gonzalo@gmail.com>
2013-12-15 13:56:57 -0300

    add date diff

--- model/encuesta_interface_test.php
+++ model/encuesta_interface_test.php
@@ -22,9 +22,25 @@ require_once "encuesta_interface.php";
 //imprimir todas las encuestas de un laboratorio para los graficos.
 $encuestas = ORM_encuesta::mostrar_encuestas_laboratorio(3);
 print_r($encuestas);
-$encuetas_nuevas = array();
-foreach ($encuestas as $key => $value) {
-  print_r($key);
+$encuestas_nuevas = array();
+
+$i = 0;
+foreach ($encuestas as $enc) {
+  $fecha_ingreso = new DateTime($enc['fecha_inicio']);
+  $fecha_analisis = new DateTime($enc['fecha_analisis']);
+  $fecha_cierre = new DateTime($enc['fecha_cierre']);
+  $fecha_recepcion = new DateTime($enc['fecha_recepcion']);
+  $fecha_ingreso = new DateTime($enc['fecha_ingreso']);
+  
+  $encuestas_nuevas[$i]['id_encuesta'] = $enc['id_encuesta']; 
+  
+  $encuestas_nuevas[$i]['tiempo_envio'] = ($fecha_recepcion->diff($fecha_inicio))['days']; 
+  $encuestas_nuevas[$i]['tiempo_analisis'] = $fecha_analisis->diff($fecha_recepcion); 
+  $encuestas_nuevas[$i]['tiempo_ingreso'] = $fecha_ingreso->diff($fecha_analisis); 
+  
+  $i++;
 }
+
+print_r($encuestas_nuevas)
 
 ?>
